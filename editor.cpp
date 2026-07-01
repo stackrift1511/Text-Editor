dc074d97889ee695226957ecf2d91985f5d910cc
stackrift._.15 <stackrift1511@gmail.com>
2026-07-01 14:45:41 +0000

    Add Terminal class with raw mode and RAII cleanup

--- editor.cpp
+++ editor.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+class Terminal {
+public:
+    Terminal(){
+        tcgetattr(STDIN_FILENO, &orig_termios);
+        struct termios raw;
+        raw = orig_termios;
+        raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    }
+
+    ~Terminal(){
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    }
+
+private:
+    struct termios orig_termios;
+};
 
 int main() {
+    Terminal term;
     std::cout << "Editor starting...\n";
     return 0;
 }
