fb5d1590483c324ac9fca8e072a3531b800c6dcf
stackrift._.15 <stackrift1511@gmail.com>
2026-07-13 08:17:10 +0000

    Add rendering loop with screen clear and vim-style tilde rows

--- editor.cpp
+++ editor.cpp
@@ -20,11 +20,29 @@ private:
     struct termios orig_termios;
 };
 
+void drawRows(){
+    for(int i=1; i<=24; i++){
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void refreshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clear screen
+    write(STDOUT_FILENO, "\x1b[H", 3);  // cursor to top-left
+    drawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);  // cursor to top-left again
+}
+
+
 void readInput(){
     while(true){
+        refreshScreen();
         char c;
         read(STDIN_FILENO, &c, 1);
         if(c=='q'){
+            //clean up the screen
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             break;
         }
         std::cout << "key: " << (int)c << " ('" << c << "')\r\n";
